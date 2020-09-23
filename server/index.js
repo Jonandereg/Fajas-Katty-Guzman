b2ba74895e2ea66732071c6310332b27f1f3f8bc
Jonandereg <jonander.eg@gmail.com>
2020-09-23 15:53:04 +0200

    server basic structure up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/router');
+const mongoose = require('./models');
+const db = mongoose.connection;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+db.on('error', console.error.bind(console, 'connection error')); //eslint-disable-line no-console
+db.once('open', () => {
+  console.log('Database in space 🛰'); //eslint-disable-line no-console
+
+  app.listen(3001, () => {
+    console.log('listening in part of space: http://localhost:3001 🛰'); //eslint-disable-line no-console
+  });
+});
